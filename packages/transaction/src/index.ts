3ec681453a32a6e23164867444fad12a66cf8bb0
Davian <kothantyarzarhein@gmail.com>
2022-05-01 20:13:48 +0630

    chore(transaction): export file of get-all-entities

--- packages/transaction/src/index.ts
+++ packages/transaction/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/base.entity';
+export * from './lib/get-all-entities';
 export * from './lib/transaction.module';
 export * from './lib/transactional-connection';
 export * from './lib/transaction.decorator';
