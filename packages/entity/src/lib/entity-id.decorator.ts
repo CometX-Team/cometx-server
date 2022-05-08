b92cab937d0846256a42d33006662d648f53c91a
Davian <kothantyarzarhein@gmail.com>
2022-05-08 11:52:19 +0630

    feat(entity): add set entity strategy method

--- packages/entity/src/lib/entity-id.decorator.ts
+++ packages/entity/src/lib/entity-id.decorator.ts
@@ -1,3 +1,19 @@
+import { Type } from '@cometx-server/common';
+import { InternalServerError } from '@cometx-server/error';
+
+interface IdColumnOptions {
+  nullable?: boolean;
+  primary?: boolean;
+}
+
+interface IdColumnConfig {
+  name: string;
+  entity: any;
+  options?: IdColumnOptions;
+}
+
+const idColumnRegistry = new Map<any, IdColumnConfig[]>();
+
 let primaryGeneratedColumn: { entity: any; name: string } | undefined;
 
 export function PrimaryGeneratedId() {
@@ -7,4 +23,35 @@ export function PrimaryGeneratedId() {
       name: propertyName,
     };
   };
+}
+
+export function EntityId(options?: IdColumnOptions) {
+  return (entity: any, propertyName: string) => {
+    const idColumns = idColumnRegistry.get(entity);
+    const entry = { name: propertyName, entity, options };
+
+    if (idColumns) {
+      idColumns.push(entry);
+    } else {
+      idColumnRegistry.set(entity, [entry]);
+    }
+  };
+}
+
+export function getIdColumns(entityType: Type<any>): IdColumnConfig[] {
+  const match = Array.from(idColumnRegistry.entries()).find(
+    ([entity, columns]) => entity.constructor === entityType,
+  );
+  return match ? match[1] : [];
+}
+
+export function getPrimaryGeneratedColumn() {
+  if (!primaryGeneratedColumn) {
+    throw new InternalServerError({
+      message:
+        'primaryGeneratedColumn is undefined.The base CometXEntity must have PrimaryGeneratedColumn decorator set on its id property.',
+    });
+  }
+
+  return primaryGeneratedColumn;
 }
