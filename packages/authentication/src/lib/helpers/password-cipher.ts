5c7a61e34323fb70b05e36f9dbe490b4c9826ea3
Davian <kothantyarzarhein@gmail.com>
2022-07-20 18:59:36 +0630

    feat(authentication): password hashing and checking functino

---
+++ packages/authentication/src/lib/helpers/password-cipher.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@cometx-server/config';
+import { PasswordHashingStrategy } from '../password-strategy/password-hashing.strategy.interface';
+
+@Injectable()
+export class PasswordCipher {
+  private passwordStrategy: PasswordHashingStrategy;
+
+  constructor(private configService: ConfigService) {
+    this.passwordStrategy =
+      this.configService.authConfig.passwordHashingStrategy;
+  }
+
+  async hash(plaintext: string): Promise<string> {
+    return await this.passwordStrategy.hash(plaintext);
+  }
+
+  async check(plaintext: string, hash: string): Promise<boolean> {
+    return await this.passwordStrategy.check(plaintext, hash);
+  }
+}
